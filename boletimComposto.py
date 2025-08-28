d2ade3357bbaa467da6b3403c53d503d1247cf11
Gabriel Vitti <vittigabriel23@gmail.com>
2025-08-28 13:46:45 -0300

    Add files via upload

--- boletimComposto.py
+++ boletimComposto.py
@@ -1,5 +1,8 @@
+# Listas
 lista = []
 boletim = []
+
+# Entrada de dados e organização das listas
 while True:
     nome = input('Digite um nome: ').capitalize().strip()
     nota1 = float(input('Digite a primeira nota: '))
@@ -14,21 +17,39 @@ while True:
     if escolha in 'N':
         break
 
+# Exposição em forma de tabela dos dados
 print('='*30)
 print(f'{'BOLETIM':^30}')
 print('='*30)
-tot = b = 0
+tot = b = num = 0
 print(f'{'Dados dos Alunos':^30}')
+print('-'*30)
 while tot<=len(boletim):
     if tot>=len(boletim):
         break
     for n in range(0, 3):
         if n==0:
+            print(f'nº{num}')
+            num+=1
             print(f'Nome: {boletim[b][n]}')
-        else:
-            print(f'Nota {n}: {boletim[b][n]}')
     media = (boletim[b][1]+boletim[b][2])/2
     print(f'Média: {media}')
     print('-'*30)
     tot+=1 
     b+=1
+
+# Mostrar as notas individuais de cada aluno
+while True:
+    num = int(input('Qual aluno você quer ver as notas? (999 interrompe): '))
+    if num==999:
+        break
+    elif num>len(boletim):
+        print('Número inválido, tente novamente...')
+        num = int(input('Qual aluno você quer ver as notas? (999 interrompe): '))
+    print('='*30)
+    print(f'{'NOTAS INDIVIDUAIS':^30}')
+    print('='*30)
+    print(f'Nome: {boletim[num][0]}')
+    print(f'Nota 1: {boletim[num][1]}')
+    print(f'Nota 2: {boletim[num][2]}')
+    print('-'*30)
